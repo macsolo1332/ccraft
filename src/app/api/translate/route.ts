d2f0060d8fd1171470608f80b647724c806c6d47
macsolo1332 <akashsuresh1332@gmail.com>
2024-04-04 13:11:12 +0530

    route handler

--- src/app/api/translate/route.ts
+++ src/app/api/translate/route.ts
@@ -35,10 +35,10 @@ export const POST = async (req: Request) => {
   })
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    // Process a POST request
-  } else {
-    // Handle any other HTTP method
-  }
-}
+// export default function handler(req: NextApiRequest, res: NextApiResponse) {
+//   if (req.method === 'POST') {
+//     // Process a POST request
+//   } else {
+//     // Handle any other HTTP method
+//   }
+// }
